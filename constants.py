45f61e0f6db156a18b0c56c6e1527ab3c11dee48
romi514 <romain.leteurtre@gmail.com>
2017-12-10 19:03:08 +0100

    Started to implement CNN

--- constants.py
+++ constants.py
@@ -3,15 +3,21 @@
 NUM_CHANNELS = 3 # RGB images
 PIXEL_DEPTH = 255
 NUM_LABELS = 2
-TRAINING_SIZE = 20
+TRAINING_SIZE = 20 # Number of images to train
 VALIDATION_SIZE = 5  # Size of the validation set.
-SEED = 66478  # Set to None for random seed.
-BATCH_SIZE = 16 # 64
+SEED = None  # Set to None for random seed.
 NUM_EPOCHS = 5
 RESTORE_MODEL = False # If True, restore existing model instead of training a new one
 RECORDING_STEP = 1000
+IMG_SIZE = 400
+
+LEARNING_RATE = 1e-6
+
+# Square size of scaled image
+SCALED_IMAGE_SIZE = 224
 
 # Set image patch size in pixels
 # IMG_PATCH_SIZE should be a multiple of 4
 # image size should be an integer multiple of this number!
+# Used to compute patch label for submission
 IMG_PATCH_SIZE = 16
--- model.py
+++ model.py
@@ -1,65 +1,169 @@
-def model(train=False):
+from constants import *
+from parameters import *
 
-    train_data_node = tf.placeholder( tf.float32, shape=(BATCH_SIZE, IMG_HEIGHT, IMG_WIDTH, NUM_CHANNELS)) # --> 400*400*3
+import tensorflow as tf
 
-    conv1 = conv_layer(train_data_node,conv1_weights,conv1_bias) # --> 400*400*32
-
-    pool1, pool1_argmax = pool_layer(conv1) # --> 200*200*32
-
-    conv2 = conv_layer(pool1, conv2_weights, conv2_bias) # --> 200*200*64
-
-    pool2, pool2_argmax = pool_layer(conv2) # --> 100*100*64
-
-    conv3 = conv_layer(pool2, conv3_weights, conv3_bias) # --> 100*100*128
-
-    pool3, pool3_argmax = pool_layer(conv3) # --> 50*50*128
-
-    conv4 = conv_layer(pool3, conv4_weights, conv4_bias) # --> 50*50*256 
-
-    pool4, pool4_argmax = pool_layer(conv4) # --> 25*25*256
-
-    fc1 = conv_layer(pool4, [, 7, 512, 4096], 4096, 'fc_6')
-    fc2 = conv_layer(fc_6, [1, 1, 4096, 4096], 4096, 'fc_7')
+# FUNCTIONS COPIED FROM CODE IN PAPER !!!! Only a few changes
 
 
+def model(data):
 
-def define_weights():
+    conv_1_1 = conv_layer(data, conv_1_1_weights, conv_1_1_bias)
+    conv_1_2 = conv_layer(conv_1_1, conv_1_2_weights, conv_1_2_bias)
 
-    conv1_weights = tf.Variable(tf.truncated_normal([5, 5, NUM_CHANNELS, 32],  # 5x5 filter, depth 32.
-                            stddev=0.1,
-                            seed=SEED))    
+    pool_1, pool_1_argmax = pool_layer(conv_1_2)
 
-    conv1_biases = tf.Variable(tf.zeros([32]))
-    
-    conv2_weights = tf.Variable(tf.truncated_normal([5, 5, 32, 64],  # 5x5 filter, depth 64.
-                            stddev=0.1,
-                            seed=SEED))    
+    conv_2_1 = conv_layer(pool_1, conv_2_1_weights, conv_2_1_bias)
+    conv_2_2 = conv_layer(conv_2_1, conv_2_2_weights, conv_2_2_bias)
 
-    conv2_biases = tf.Variable(tf.zeros([64]))
+    pool_2, pool_2_argmax = pool_layer(conv_2_2)
 
-    conv3_weights = tf.Variable(tf.truncated_normal([5, 5, 64, 128],  # 5x5 filter, depth 128.
-                            stddev=0.1,
-                            seed=SEED))    
+    conv_3_1 = conv_layer(pool_2, conv_3_1_weights, conv_3_1_bias)
+    conv_3_2 = conv_layer(conv_3_1, conv_3_2_weights, conv_3_2_bias)
+    conv_3_3 = conv_layer(conv_3_2, conv_3_3_weights, conv_3_3_bias)
 
-    conv3_biases = tf.Variable(tf.zeros([128]))
-    
-    conv4_weights = tf.Variable(tf.truncated_normal([5, 5, 128, 256],  # 5x5 filter, depth 256.
-                            stddev=0.1,
-                            seed=SEED))    
+    pool_3, pool_3_argmax = pool_layer(conv_3_3)
 
-    conv4_biases = tf.Variable(tf.zeros([256]))
+    conv_4_1 = conv_layer(pool_3, conv_4_1_weights, conv_4_1_bias)
+    conv_4_2 = conv_layer(conv_4_1, conv_4_2_weights, conv_4_2_bias)
+    conv_4_3 = conv_layer(conv_4_2, conv_4_3_weights, conv_4_3_bias)
 
-    fc1_weights = tf.Variable(tf.truncated_normal([5, 5, 128, 256],  # 25x25 filter, depth .
-                            stddev=0.1,
-                            seed=SEED))   
+    pool_4, pool_4_argmax = pool_layer(conv_4_3)
+
+    conv_5_1 = conv_layer(pool_4, conv_5_1_weights, conv_5_1_bias)
+    conv_5_2 = conv_layer(conv_5_1, conv_5_2_weights, conv_5_2_bias)
+    conv_5_3 = conv_layer(conv_5_2, conv_5_3_weights, conv_5_3_bias)
+
+    pool_5, pool_5_argmax = pool_layer(conv_5_3)
+
+    fc_6 = conv_layer(pool_5, fc_6_weights, fc_6_bias)
+    fc_7 = conv_layer(fc_6, fc_7_weights, fc_7_bias)
+
+    deconv_fc_6 = deconv_layer(fc_7, deconv_fc_6_weights, deconv_fc_6_bias)
+
+    unpool_5 = unpool_layer2x2(deconv_fc_6, pool_5_argmax, tf.shape(conv_5_3))
+
+    deconv_5_3 = deconv_layer(unpool_5, deconv_5_3_weights, deconv_5_3_bias)
+    deconv_5_2 = deconv_layer(deconv_5_3, deconv_5_2_weights, deconv_5_2_bias)
+    deconv_5_1 = deconv_layer(deconv_5_2, deconv_5_1_weights, deconv_5_1_bias)
+
+    unpool_4 = unpool_layer2x2(deconv_5_1, pool_4_argmax, tf.shape(conv_4_3))
+
+    deconv_4_3 = deconv_layer(unpool_4, deconv_4_3_weights, deconv_4_3_bias)
+    deconv_4_2 = deconv_layer(deconv_4_3, deconv_4_2_weights, deconv_4_2_bias)
+    deconv_4_1 = deconv_layer(deconv_4_2, deconv_4_1_weights, deconv_4_1_bias)
+
+    unpool_3 = unpool_layer2x2(deconv_4_1, pool_3_argmax, tf.shape(conv_3_3))
+
+    deconv_3_3 = deconv_layer(unpool_3, deconv_3_3_weights, deconv_3_3_bias)
+    deconv_3_2 = deconv_layer(deconv_3_3, deconv_3_2_weights, deconv_3_2_bias)
+    deconv_3_1 = deconv_layer(deconv_3_2, deconv_3_1_weights, deconv_3_1_bias)
+
+    unpool_2 = unpool_layer2x2(deconv_3_1, pool_2_argmax, tf.shape(conv_2_2))
+
+    deconv_2_2 = deconv_layer(unpool_2, deconv_2_2_weights, deconv_2_2_bias)
+    deconv_2_1 = deconv_layer(deconv_2_2, deconv_2_1_weights, deconv_2_1_bias)
+
+    unpool_1 = unpool_layer2x2(deconv_2_1, pool_1_argmax, tf.shape(conv_1_2))
+
+    deconv_1_2 = deconv_layer(unpool_1, deconv_1_2_weights, deconv_1_2_bias)
+    deconv_1_1 = deconv_layer(deconv_1_2, deconv_1_1_weights, deconv_1_1_bias)
+
+    score = deconv_layer(deconv_1_1, score_weights, score_bias)
+
+    return score
 
 
+def conv_layer(x, weights, bias, padding='SAME'):
+    return tf.nn.relu(tf.nn.conv2d(x, weights, strides=[1, 1, 1, 1], padding=padding) + bias)
 
 def pool_layer(x):
-
     return tf.nn.max_pool_with_argmax(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
 
-def conv_layer(x,weights,bias,padding='SAME'):
+def deconv_layer(x, weights, bias, padding='SAME'):
 
-    return f.nn.relu(tf.nn.conv2d(x, weights, strides=[1, 1, 1, 1], padding=padding) + bias)
+    x_shape = tf.shape(x)
+    out_shape = tf.stack([x_shape[0], x_shape[1], x_shape[2], W_shape[2]])
+
+    return tf.nn.conv2d_transpose(x, weights, out_shape, [1, 1, 1, 1], padding=padding) + bias
+
+def unravel_argmax(argmax, shape):
+    output_list = []
+    output_list.append(argmax // (shape[2] * shape[3]))
+    output_list.append(argmax % (shape[2] * shape[3]) // shape[3])
+    return tf.stack(output_list)
+
+def unpool_layer2x2(x, raveled_argmax, out_shape):
+    argmax = unravel_argmax(raveled_argmax, tf.to_int64(out_shape))
+    output = tf.zeros([out_shape[1], out_shape[2], out_shape[3]])
+
+    height = tf.shape(output)[0]
+    width = tf.shape(output)[1]
+    channels = tf.shape(output)[2]
+
+    t1 = tf.to_int64(tf.range(channels))
+    t1 = tf.tile(t1, [((width + 1) // 2) * ((height + 1) // 2)])
+    t1 = tf.reshape(t1, [-1, channels])
+    t1 = tf.transpose(t1, perm=[1, 0])
+    t1 = tf.reshape(t1, [channels, (height + 1) // 2, (width + 1) // 2, 1])
+
+    t2 = tf.squeeze(argmax)
+    t2 = tf.stack((t2[0], t2[1]), axis=0)
+    t2 = tf.transpose(t2, perm=[3, 1, 2, 0])
+
+    t = tf.concat([t2, t1], 3)
+    indices = tf.reshape(t, [((height + 1) // 2) * ((width + 1) // 2) * channels, 3])
+
+    x1 = tf.squeeze(x)
+    x1 = tf.reshape(x1, [-1, channels])
+    x1 = tf.transpose(x1, perm=[1, 0])
+    values = tf.reshape(x1, [-1])
+
+    delta = tf.SparseTensor(indices, values, tf.to_int64(tf.shape(output)))
+    return tf.expand_dims(tf.sparse_tensor_to_dense(tf.sparse_reorder(delta)), 0)
+
+def unpool_layer2x2_batch(x, argmax):
+    '''
+    Args:
+        x: 4D tensor of shape [batch_size x height x width x channels]
+        argmax: A Tensor of type Targmax. 4-D. The flattened indices of the max
+        values chosen for each output.
+    Return:
+        4D output tensor of shape [batch_size x 2*height x 2*width x channels]
+    '''
+    x_shape = tf.shape(x)
+    out_shape = [x_shape[0], x_shape[1]*2, x_shape[2]*2, x_shape[3]]
+
+    batch_size = out_shape[0]
+    height = out_shape[1]
+    width = out_shape[2]
+    channels = out_shape[3]
+
+    argmax_shape = tf.to_int64([batch_size, height, width, channels])
+    argmax = unravel_argmax(argmax, argmax_shape)
+
+    t1 = tf.to_int64(tf.range(channels))
+    t1 = tf.tile(t1, [batch_size*(width//2)*(height//2)])
+    t1 = tf.reshape(t1, [-1, channels])
+    t1 = tf.transpose(t1, perm=[1, 0])
+    t1 = tf.reshape(t1, [channels, batch_size, height//2, width//2, 1])
+    t1 = tf.transpose(t1, perm=[1, 0, 2, 3, 4])
+
+    t2 = tf.to_int64(tf.range(batch_size))
+    t2 = tf.tile(t2, [channels*(width//2)*(height//2)])
+    t2 = tf.reshape(t2, [-1, batch_size])
+    t2 = tf.transpose(t2, perm=[1, 0])
+    t2 = tf.reshape(t2, [batch_size, channels, height//2, width//2, 1])
+
+    t3 = tf.transpose(argmax, perm=[1, 4, 2, 3, 0])
+
+    t = tf.concat([t2, t3, t1], 4)
+    indices = tf.reshape(t, [(height//2)*(width//2)*channels*batch_size, 4])
+
+    x1 = tf.transpose(x, perm=[0, 3, 1, 2])
+    values = tf.reshape(x1, [-1])
+
+    delta = tf.SparseTensor(indices, values, tf.to_int64(out_shape))
+
+    return tf.sparse_tensor_to_dense(tf.sparse_reorder(delta))
