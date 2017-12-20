7fd7483ea24696cfe734dbedf46b92876e1cc084
besma-elk <besmaelk@gmail.com>
2017-12-20 14:35:43 +0100

    generator

--- cnn_with_sliding_window.py
+++ cnn_with_sliding_window.py
@@ -90,44 +90,15 @@ class Cnn:
         
         #data augmentation
         def generator(data, labels):
-           
-            while True:
-                data_batch = np.zeros((batch_size, window_size, window_size, 3))
-                labels_batch = np.zeros((batch_size, 2))      
-         
+            ### TODO
 
-                for i in range(batch_size):
-                    img = np.random.choice(X.shape[0])
-                    w_half = int(window_size/2)
-                    p_half = int(patch_size/2)
-                    origin = np.random.randint(w_half, X[img].shape[0] - w_half, 2)
+        
+      
 
-                    x_window = X[img][origin[0]-w_half:origin[0]+w_half, origin[1]-w_half:origin[1]+w_half]
-                    y_window = Y[img][origin[0]-p_half:origin[0]+p_half, origin[1]-p_half:origin[1]+p_half ]
-                    
-                    #construct the labels using a foreground_threshold (replace >True by 1)
-                    y_window = 1*(np.array([np.mean(y_window)]) > 0.25)
-
-                    
-                    if np.random.random() < 0.5:
-                        x_window = np.flipud(x_window)
-
-                    if np.random.random() < 0.5:
-                        x_window = np.fliplr(x_window)
-                    
-                    data_batch[i] = x_window
-                    labels_batch[i] = y_window
-                
-                    
-                yield (data_batch, labels_batch)
-
-        try:
-
-            self.model.fit_generator(generator(data,labels),validation_data=generator(data,labels),validation_steps=0.1,
-                            samples_per_epoch=10,
+        self.model.fit_generator(generator(data,labels),validation_data=generator(data,labels),validation_steps=0.1,
+                          samples_per_epoch=10,
                             nb_epoch=20,
                             verbose=2,
                                    )
-        except KeyboardInterrupt:
-            pass
+
 
